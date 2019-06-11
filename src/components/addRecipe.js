7096455123135eac6a0ed59d70f41f9dd9183dc2
michalsinu <michalsinu@gmail.com>
2019-06-11 19:08:44 +0200

    first try on the add recipe page

--- src/components/addRecipe.js
+++ src/components/addRecipe.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router'
+import { connect } from 'react-redux';
+import { fetchRecipes } from '../actions/recipesActions';
+import { Link } from "react-router-dom";
+
+import Error from './error';
 
 class addRecipe extends Component {
   render () {
     return (
-      <h1>Hello</h1>
+    <React.Fragment>
+      <div className="heading">
+        <div className="container-fluid row">
+          <div className="col-2">
+            <Link to="/"><span className="fa fa-add fa-arrow-left"></span></Link>
+          </div>
+
+          <div className="col-8">
+            Přidat recept
+          </div>
+
+          <div className="col-2">
+            <Link to="/addrecipe"><span className="fa fa-add fa-plus"></span></Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="content">
+       <form>
+         <div className="form-group">
+          <input type="text" className="form-control" id="Název receptu" placeholder="Název receptu" />
+        </div>
+
+          <div className="form-group">
+            <input type="text" className="form-control" id="Uvodní text" placeholder="Uvodní text" />
+          </div>
+
+        </form>
+      </div>
+    </React.Fragment>
     );
   }
 }
