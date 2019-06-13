768f7d81104397c058081f0b48f70dfa60c0e19b
michalsinu <michalsinu@gmail.com>
2019-06-13 02:16:00 +0200

    add recipe work 2.0

--- src/components/addRecipe.js
+++ src/components/addRecipe.js
@@ -7,6 +7,28 @@ import { Link } from "react-router-dom";
 import Error from './error';
 
 class addRecipe extends Component {
+  componentDidMount() {
+    function setFocused(event) {
+      var results = document.querySelectorAll('.input-smallheading');
+
+        console.log("focused!" + event.target.placeholder);
+
+    }
+
+    function unsetFocused(event) {
+      var results = document.querySelectorAll('.input-smallheading');
+          console.log("unfocused" + event.target.placeholder);
+    }
+
+    var results = document.getElementsByClassName('input-addrecipe');
+
+      for (var i = 0; results.length > i; i++) {
+       results[i].addEventListener("focus", (e) => setFocused(e));
+       results[i].addEventListener("blur",  (e) => unsetFocused(e));
+
+     }
+  }
+
   render () {
     return (
     <React.Fragment>
@@ -26,20 +48,24 @@ class addRecipe extends Component {
         </div>
       </div>
 
-      <div className="content">
+      <div className="content-addrecipe">
        <form>
          <div className="form-group">
-          <input type="text" className="form-control" id="Název receptu" placeholder="Název receptu" />
+          <div className="input-smallheading">Název receptu</div>
+          <input type="text" className="form-control input-addrecipe" placeholder="Název receptu" />
         </div>
 
           <div className="form-group">
-            <input type="text" className="form-control" id="Uvodní text" placeholder="Uvodní text" />
+            <div className="input-smallheading">Uvodní text</div>
+            <input type="text" className="form-control input-addrecipe" placeholder="Uvodní text" />
           </div>
 
         </form>
       </div>
     </React.Fragment>
     );
+
+
   }
 }
 
