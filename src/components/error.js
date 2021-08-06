ad1211e0ce551457acf4973e8acbb4152d167fed
Michal Sinu <michalsinu@gmail.com>
2021-08-06 02:34:59 +0200

    updated modules

--- src/components/error.js
+++ src/components/error.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import createHistory from "history/createBrowserHistory";
-
-require("history").createBrowserHistory
 
 class error extends Component {
   render() {
-    const history = createHistory()
-
     return (
       <div className="error">
         <div className="error_content">
           <h1>{this.props.status.code}</h1>
           {this.props.status.text} <br />
 
-          <Link onClick={history.goBack()}>GO BACK</Link>
         </div>
       </div>
     );
